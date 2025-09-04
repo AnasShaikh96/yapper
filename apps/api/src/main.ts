f3f5127c9292376fc53e6437dbcb0f9f787ca498
Anas Shaikh <shaikhanas246@gmail.com>
2025-09-04 16:04:08 +0530

    add barebones and folder structure

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,35 @@
 
 import express from 'express';
 import * as path from 'path';
+import cors from "cors"
+import ApiError from './utils/ApiError';
+import status from 'http-status';
+
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.use(cors())
+
+
+// MIDDLEWARES
+
+
+// ROUTES
+
+
+// ERRORHANDLERS
+
+
+
+// send back a 404 error for any unknown api request
+app.use((req, res, next) => {
+  next(new ApiError(status.NOT_FOUND, 'Not found'));
 });
 
 const port = process.env.PORT || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
---
+++ apps/api/src/utils/ApiError.ts
@@ -0,0 +1,27 @@
+class ApiError extends Error {
+    public statusCode: number;
+    public isOperational: boolean;
+  
+    constructor(
+      statusCode: number,
+      message: string,
+      isOperational: boolean = true,
+      stack: string = ''
+    ) {
+      super(message);
+  
+      Object.setPrototypeOf(this, new.target.prototype); // restore prototype chain
+  
+      this.statusCode = statusCode;
+      this.isOperational = isOperational;
+  
+      if (stack) {
+        this.stack = stack;
+      } else {
+        Error.captureStackTrace(this, this.constructor);
+      }
+    }
+  }
+  
+  export default ApiError;
+  
